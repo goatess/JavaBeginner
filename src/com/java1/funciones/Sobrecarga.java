caee495fbb248a67d4b85c3dca98ba1e0e392378
goatess <107187023+goatess@users.noreply.github.com>
2022-07-25 19:04:06 +0200

    ejercicio tema 4

---
+++ src/com/java1/funciones/Sobrecarga.java
@@ -0,0 +1,14 @@
+package com.java1.funciones;
+
+public class Sobrecarga {
+    
+    static double sum (double number1, double number2){
+        return number1 + number2;
+    }
+    static int sum (int number1, int number2){
+        return number1 + number2;
+    }
+    static int sum (int number1, int number2, int number3){
+        return number1 + number2;
+    }   
+}
