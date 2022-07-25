b3552690e18980c8558cda11779d56d0b9cb9f33
goatess <107187023+goatess@users.noreply.github.com>
2022-07-25 14:19:48 +0200

    tema 3 funciones

---
+++ src/com/tipos/Sobrecarga.java
@@ -0,0 +1,14 @@
+package com.tipos;
+
+public class Sobrecarga {
+    
+    static double sum (double number1, double number2){
+        return number1 + number2;
+    }
+    static int sum (int number1, int number2){
+        return number1 + number2;
+    }
+    static int sum (int number1, int number2, int number3){
+        return number1 + number2;
+    }   
+}
